74d091d29b825879d2e33c75d7e8b22c1d1cd947
Javier Ruiz <javiruizz.jr@gmail.com>
2022-11-21 20:19:51 +0100

    init front

---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Login from '../pages/login/Login';
+import SignUp from '../pages/sign-up/SignUp';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const AppRouter = () => {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Login />
+    },
+    {
+      path: '/login',
+      element: <Login />
+    },
+    {
+      path: '/routes',
+      element: <Login />
+    },
+    {
+      path: '/sign-up',
+      element: <SignUp />
+    }
+  ])
+
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default AppRouter;
